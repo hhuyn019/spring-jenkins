5a3da8dcf6c7e9ccebb661c540cca2b22b583c4d
Chris Huynh <hhuyn019@ucr.edu>
2024-07-01 13:48:42 -0700

    first commit

---
+++ src/test/java/com/cogent/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cogent.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@Test
+	void contextLoads() {
+		log.info("Test case execution");
+	}
+
+}
